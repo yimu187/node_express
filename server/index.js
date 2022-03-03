41ed7304b6a5e93c9052b4c92adb37116b224095
Murat YILMAZ <murat.yilmaz@siemens.com>
2022-03-03 19:05:47 +0300

    initial commit

---
+++ server/index.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import cors from 'cors';
+import process from 'process';
+
+const app = express();
+
+app.use(cors({
+origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    // if(allowedOrigins.indexOf(origin) === -1){
+    // var msg = 'The CORS policy for this site does not ' +
+    //             'allow access from the specified Origin.';
+    // return callback(new Error(msg), false);
+    // }
+    return callback(null, true);
+}
+}));
+
+app.get('/health',(req,res) => {
+    console.log('receiving get request');
+    const cmd = './docker.sh ';
+
+    const resp = {
+        state: 'Up and Running',
+        processId : process.pid
+    }
+    res.send(resp);
+    return;
+});
+
+const PORT = 5000;
+
+app.listen(PORT,() => {
+    console.log(`Running on PORT ${PORT}`);
+});
